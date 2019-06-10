8baf5a9278c55774ace69c7e6d2e2eebe340f123
dsfdarlan-32 <dsfdarlan-32@hotmail.com>
2019-06-10 17:39:49 -0300

    subir

--- getaoDeEstoque/src/main/java/com/getaoDeEstoque/getaoDeEstoque/controlleres/ProjetoController.java
+++ getaoDeEstoque/src/main/java/com/getaoDeEstoque/getaoDeEstoque/controlleres/ProjetoController.java
@@ -6,9 +6,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class ProjetoController {
   
-  @RequestMapping("/CadastrarProjeto")
+  @RequestMapping("/cadastrarProjeto")
   public String formCadastrarProjeto() {
-    return "projeto/CadastrarProjeto";
+    return "projeto/cadastrarProjeto";
   }
     
 }
