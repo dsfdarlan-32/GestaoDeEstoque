481950b55b8fb201d8e9724c465f6c3d5157d639
dsfdarlan-32 <dsfdarlan-32@hotmail.com>
2019-06-15 10:27:15 -0300

    commit

--- getaoDeEstoque/src/main/java/com/getaoDeEstoque/getaoDeEstoque/models/fornecedor.java
+++ getaoDeEstoque/src/main/java/com/getaoDeEstoque/getaoDeEstoque/models/fornecedor.java
@@ -1,18 +1,35 @@
 package com.getaoDeEstoque.getaoDeEstoque.models;
 
-public class fornecedor {
-  private String nome;
-  private String codigo;
-  public String getNome() {
-    return nome;
-  }
-  public void setNome(String nome) {
-    this.nome = nome;
-  }
-  public String getCodigo() {
-    return codigo;
-  }
-  public void setCodigo(String codigo) {
-    this.codigo = codigo;
-  }
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class fornecedor implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	//@GeneratedValue(strategy = GenerationType.AUTO)
+	private String codigo;
+	private String nome;
+	
+	public String getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(String codigo) {
+		this.codigo = codigo;
+	}
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
 }
